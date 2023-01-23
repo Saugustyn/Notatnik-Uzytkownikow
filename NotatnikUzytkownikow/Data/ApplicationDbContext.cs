77562752de44ed66c05aa2c97113c1237837ac12
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-23 12:18:26 +0100

    Add ApplicationDbContext and Connection String

---
+++ NotatnikUzytkownikow/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotatnikUzytkownikow.Models;
+
+namespace NotatnikUzytkownikow.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
