6d3ae31b4d166c8c6dfb3d76781c52cedf7cd7b7
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-23 13:22:46 +0100

    Add Index

---
+++ NotatnikUzytkownikow/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotatnikUzytkownikow.Data;
+using NotatnikUzytkownikow.Models;
+using System.Diagnostics;
+
+namespace NotatnikUzytkownikow.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PersonController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Person> objPersonList = _db.Persons;
+            return View(objPersonList);
+        }
+    }
+}
